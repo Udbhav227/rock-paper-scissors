3b3ccdeab1c1b596ef019f6907884e5e99ba73ad
Udbhav <Udbhav227@gmail.com>
2024-07-15 22:24:41 +0530

    Added score counter in html and asjusted script

--- script.js
+++ script.js
@@ -1,41 +1,48 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanScoreDiv = document.querySelector('.player-score');
+const computerScoreDiv = document.querySelector('.computer-score');
+
 function getComputerChoice() {
     let rand = Math.floor(3 * Math.random());
-    switch(rand) {
+    switch (rand) {
         case 0: return "rock";
         case 1: return "paper";
         case 2: return "scissors";
     }
-} 
-
-function getHumanChoice() {
-    return prompt("Choose one: Rock, Paper or Scissors".toLowerCase()); 
 }
 
-let humanScore = 0;
-let computerScore = 0;
-
 function playRound(humanChoice, computerChoice) {
+    const resultDiv = document.querySelector('.result');
+
     if (humanChoice == computerChoice) {
-        console.log("Draw! Both chose " + humanChoice + ".");;
+        console.log("Draw! Both chose " + humanChoice + ".");
+        resultDiv.textContent = "Draw! Both chose " + humanChoice + ".";
     }
-    else if (humanChoice == "rock" && computerChoice == "scissors" ||
+    else if (
+        humanChoice == "rock" && computerChoice == "scissors" ||
         humanChoice == "paper" && computerChoice == "rock" ||
-        humanChoice == "scissors" && computerChoice == "paper") {
-            humanScore++;
-            console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
-        }
+        humanChoice == "scissors" && computerChoice == "paper"
+    ) {
+        humanScore++;
+        console.log("You win! " + humanChoice + " beats " + computerChoice + ".");
+        resultDiv.textContent = "You win! " + humanChoice + " beats " + computerChoice + ".";
+    }
     else {
         computerScore++;
         console.log("Computer wins! " + computerChoice + " beats " + humanChoice + ".")
+        resultDiv.textContent = "Computer wins! " + computerChoice + " beats " + humanChoice + ".";
     }
+    humanScoreDiv.textContent = "Player: " + humanScore;
+    computerScoreDiv.textContent = "Computer: " + computerScore;
 }
 
-const btn = document.querySelector(button);
+const btns = document.querySelectorAll("button");
 
-btn.addEventListener('click', () => {
-    switch(btn.className) {
-        case 'rock' : playRound("rock", getComputerChoice());
-        case 'paper' : playRound("paper", getComputerChoice());
-        case 'scissors' : playRound("scissors", getComputerChoice());
-    }
+btns.forEach(button => {
+    button.addEventListener('click', () => {
+        const humanChoice = button.className;
+        playRound(humanChoice, getComputerChoice());
+    });
 });
